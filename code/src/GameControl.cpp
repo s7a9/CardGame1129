2c5e73a129100d31b5a097254f1f731a4b7f55ff
s7a9 <dch_7723@outlook.com>
2022-12-01 14:49:15 +0800

    更新

--- code/src/GameControl.cpp
+++ code/src/GameControl.cpp
@@ -10,8 +10,23 @@ const card_t player_inital_cards[] = {
     {ct_attack, 2, 0}, {ct_attack, 2, 0}, {ct_attack, 2, 0}, 
 };
 
-void SpawnEnemyCardBag() {
+int get_hp_by_level(int level) {
+    return 10 + 5 * level;
+}
+
+int get_max_hand_by_level() {
     
+}
+
+card_t GetNewCard(card_type_t type, int level) {
+    // 每种卡牌对应的行动点花费
+    const int type_ap_cost[] = {0, 1, 1, 2, 2, 1, 2, 2, 2, 2};
+    // 每种卡牌对应的基础值
+    const int type_basic_value[] = {0, 3, 1, 2, 2, 2, 0, 1, 1, 0};
+    return Card(type, type_basic_value[type] + level, type_ap_cost[type]);
+}
+
+void SpawnEnemyCardBag() {
 }
 
 void RunGame() {
@@ -32,14 +47,19 @@ void Fight() {
     choice = MakeAChoice(enemy_level_options, 3);
     switch (choice) {
     case 1:
-        game_status.enemy.level = max(1, game_status.player)
+        game_status.enemy.level = max(1, game_status.player.level - 1);
         break;
-    
+    case 3:
+        game_status.enemy.level = game_status.player.level + 1;
+        break;
     default:
+        game_status.enemy.level = game_status.player.level;
         break;
     }
+    SpawnEnemyCardBag();
     while (true) {
         // 玩家回合 发牌、计算效果
+
         while (true) {
             // 玩家打牌
             
